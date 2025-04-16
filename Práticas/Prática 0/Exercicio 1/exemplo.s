7ee690b6184e9f7fa5917af23884fbcc4fe21ffb
VM <82391099+Vitroror@users.noreply.github.com>
2025-04-15 23:24:12 -0300

    exemplo.s otimizado para menos instruções

--- Práticas/Prática 0/Exercicio 1/exemplo.s
+++ Práticas/Prática 0/Exercicio 1/exemplo.s
@@ -6,57 +6,42 @@
 ; Prof. DaLuz           		- 25/02/2022
 
 ;################################################################################
-; Declarações EQU
+; DeclaraÃ§Ãµes EQU
 ; <NOME>	EQU <VALOR>
 ;################################################################################
 	AREA    |.text|, CODE, READONLY, ALIGN=2
 	THUMB
-; Se alguma função do arquivo for chamada em outro arquivo	
-    EXPORT Start					; Permite chamar a função Start a partir de 
+; Se alguma funÃ§Ã£o do arquivo for chamada em outro arquivo	
+    EXPORT Start					; Permite chamar a funÃ§Ã£o Start a partir de 
 									; outro arquivo. No caso startup.s
 								
-; Se chamar alguma função externa	
+; Se chamar alguma funÃ§Ã£o externa	
 ;	IMPORT <func>          			; Permite chamar dentro deste arquivo uma 
-									; função <func>
+									; funÃ§Ã£o <func>
 
 ;################################################################################
-; Função main()
+; FunÃ§Ã£o main()
 Start								;Label Start ... void main(void)
-; Comece o código aqui <=========================================================
-	mov r0, #65				;a) Salvar no registrador R0 o valor 65 decimal
-	mov r1, #0x1B001B00		;b) Salvar no registrador R1 o valor 0x1B00.1B00
-	mov r2, #0x5678			;c) Salvar no registrador R2 o valor 0x1234.5678
-	movt r2, #0x1234		
-	
-	mov r3, #0x0040			;d) Guardar na posição de memória 0x2000.0040 o valor de R0
-	movt r3, #0x2000
+; Comece o cÃ³digo aqui <=========================================================
+	movs r0, #65				;a) Salvar no registrador R0 o valor 65 decimal
+	ldr r1, =0x1B001B00			;b) Salvar no registrador R1 o valor 0x1B00.1B00
+	ldr r2, =0x12345678			;c) Salvar no registrador R2 o valor 0x1234.5678		
+	ldr r3, =0x20000040			;d) Guardar na posiÃ§Ã£o de memÃ³ria 0x2000.0040 o valor de R0
 	str r0, [r3]
-	
-	mov r4, #0x0044			;e) Guardar na posição de memória 0x2000.0044 o valor de R1
-	movt r4, #0x2000
+	ldr r4, =0x20000044			;e) Guardar na posiÃ§Ã£o de memÃ³ria 0x2000.0044 o valor de R1
 	str r1, [r4]
-	
-	mov r5, #0x0048			;f) Guardar na posição de memória 0x2000.0048 o valor de R2
-	movt r5, #0x2000
+	ldr r5, =0x20000048			;f) Guardar na posiÃ§Ã£o de memÃ³ria 0x2000.0048 o valor de R2
 	str r2, [r5]
-	
-	mov r6, #0x0001			;g) Guardar na posição de memória 0x2000.004C o número 0xF0001
-	movt r6, #0x000F
-	mov r7, #0x004C
-	movt r7, #0x2000
+	ldr r6, =0xF0001			;g) Guardar na posiÃ§Ã£o de memÃ³ria 0x2000.004C o nÃºmero 0xF0001
+	ldr r7, =0x2000004C
 	str r6, [r7]
-	
-	mov r8, #0xCD			;h) Guardar na posição de memória 0x2000.0046 o byte 0xCD, sem sobrescrever os outros bytes da WORD
-	mov r9, #0x0046
-	movt r9, #0x2000
+	movs r8, #0xCD				;h) Guardar na posiÃ§Ã£o de memÃ³ria 0x2000.0046 o byte 0xCD, sem sobrescrever os outros bytes da WORD
+	ldr r9, =0x20000046
 	strb r8, [r9]
-	
-	ldr r10, [r3]			;i) Ler o conteúdo da memória cuja posição 0x2000.0040 e guardar no R7
-	ldr r11, [r5]			;j) Ler o conteúdo da memória cuja posição 0x2000.0048 o guardar R8
-	mov r12, r10			;k) Copiar para o R9 o conteúdo de R7
-
-
-; Final do código aqui <=========================================================
+	ldr r10, [r3]				;i) Ler o conteÃºdo da memÃ³ria cuja posiÃ§Ã£o 0x2000.0040 e guardar no R7
+	ldr r11, [r5]				;j) Ler o conteÃºdo da memÃ³ria cuja posiÃ§Ã£o 0x2000.0048 o guardar R8
+	mov r12, r10				;k) Copiar para o R9 o conteÃºdo de R7
+; Final do cÃ³digo aqui <=========================================================
     NOP
-    ALIGN                       	;garante que o fim da seção está alinhada 
+    ALIGN                       	;garante que o fim da seÃ§Ã£o estÃ¡ alinhada 
     END                         	;fim do arquivo
