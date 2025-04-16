12d7400f0c45db89acc4b9d5cd5aecb494787a40
VM <82391099+Vitroror@users.noreply.github.com>
2025-04-16 14:08:10 -0300

    exercicio 2 otimizado

--- Práticas/Prática 0/Exercicio 2/exemplo.s
+++ Práticas/Prática 0/Exercicio 2/exemplo.s
@@ -6,52 +6,36 @@
 ; Prof. DaLuz           		- 25/02/2022
 
 ;################################################################################
-; Declarações EQU
+; DeclaraÃ§Ãµes EQU
 ; <NOME>	EQU <VALOR>
 ;################################################################################
 	AREA    |.text|, CODE, READONLY, ALIGN=2
 	THUMB
-; Se alguma função do arquivo for chamada em outro arquivo	
-    EXPORT Start					; Permite chamar a função Start a partir de 
+; Se alguma funÃ§Ã£o do arquivo for chamada em outro arquivo	
+    EXPORT Start					; Permite chamar a funÃ§Ã£o Start a partir de 
 									; outro arquivo. No caso startup.s
 								
-; Se chamar alguma função externa	
+; Se chamar alguma funÃ§Ã£o externa	
 ;	IMPORT <func>          			; Permite chamar dentro deste arquivo uma 
-									; função <func>
+									; funÃ§Ã£o <func>
 
 ;################################################################################
-; Função main()
+; FunÃ§Ã£o main()
 Start								;Label Start ... void main(void)
-; Comece o código aqui <=========================================================
-	mov r12, #0xF0				;a) Realizar a operação lógica AND do valor 0xF0 com o valor binário 
-	ands r0, r12, #2_01010101	;01010101 e salvar o resultado em R0. Utilizar o sufixo ‘S’ para atualizar 
+; Comece o cÃ³digo aqui <=========================================================
+	mov r12, #0xF0				;a) Realizar a operaÃ§Ã£o lÃ³gica AND do valor 0xF0 com o valor binÃ¡rio 
+	ands r0, r12, #2_01010101	;01010101 e salvar o resultado em R0. Utilizar o sufixo â€˜Sâ€™ para atualizar 
 								;os flags
-
-	mov r11,  #2_11001100		;b) Realizar a operação lógica AND do valor 11001100 binário com o valor 
-	ands r1, r11, #2_00110011	;binário 00110011 e salvar o resultado em R1. Utilizar o sufixo ‘S’ para 
+	mov r11,  #2_11001100		;b) Realizar a operaÃ§Ã£o lÃ³gica AND do valor 11001100 binÃ¡rio com o valor 
+	ands r1, r11, #2_00110011	;binÃ¡rio 00110011 e salvar o resultado em R1. Utilizar o sufixo â€˜Sâ€™ para 
 								;atualizar os flags
-	
-	
-	mov r10, #2_10000000		;c) Realizar a operação lógica OR do valor 10000000 binário com o valor 
-	orrs r2, r10, #2_00110111	;binário 00110111 e salvar o resultado em R2. Utilizar o sufixo ‘S’ para 
-								;atualizar os flags
-	
-	
-	mov r9, #0xABCD				;d)Realizar a operação lógica AND do valor 0xABCDABCD com o valor 
-	movt r9, #0xABCD			;0xFFFF0000 (sem usar LDR-diretiva) e salvar o resultado em R3. Utilizar 
-	mov r8, #0xFFFF				;o sufixo ‘S’ para atualizar os flags. Utilizar a instrução BIC;
-	bics r3, r9, r8
-	
-	
-	
-	
-	
-	
-	
-	
-
-
-; Final do código aqui <=========================================================
+	mov r10, #2_10000000		;c) Realizar a operaÃ§Ã£o lÃ³gica OR do valor 10000000 binÃ¡rio com o valor 
+	orrs r2, r10, #2_00110111	;binÃ¡rio 00110111 e salvar o resultado em R2. Utilizar o sufixo â€˜Sâ€™ para 
+								;atualizar os flags	
+	movw r9, #0xABCD			;d)Realizar a operaÃ§Ã£o lÃ³gica AND do valor 0xABCDABCD com o valor
+	movt r9, #0xABCD     		;0xFFFF0000 (sem usar LDR-diretiva) e salvar o resultado em R3. Utilizar
+	bics r3, r9, #0xFFFF 		;o sufixo â€˜Sâ€™ para atualizar os flags. Utilizar a instruÃ§Ã£o BIC;
+; Final do cÃ³digo aqui <=========================================================
     NOP
-    ALIGN                       	;garante que o fim da seção está alinhada 
+    ALIGN                       	;garante que o fim da seÃ§Ã£o estÃ¡ alinhada 
     END                         	;fim do arquivo
